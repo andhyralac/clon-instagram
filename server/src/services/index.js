495084cd6a545983b19284195a217069dd3dc67b
Andhy Ralac <andhyralac11@gmail.com>
2023-04-21 13:57:43 -0600

    the import and export of post.service.js was done with an alis in the index.js

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,3 +1,4 @@
 export * as userService from './user.service.js'
 export * as authService from './auth.service.js'
 export * as cloudinaryService from './cloudinary.service.js'
+export * as postService from './post.service.js'
