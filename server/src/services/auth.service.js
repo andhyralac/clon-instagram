8010b7df0880e689bf9ee6554b516a715a22bbee
Andhy Ralac <andhyralac11@gmail.com>
2023-04-21 11:32:30 -0600

    The names of the token and password imports were updated

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,16 +1,16 @@
 import { userService } from './index.js'
-import { token, password } from '../helpers/index.js'
+import { handleToken, handlePassword } from '../helpers/index.js'
 
 
 export const auth = async (email, passwordText) => {
     const user = await userService.findOneUser({ email })
-    const isPasswordMatch = password.compare(passwordText, user.password)
+    const isPasswordMatch = handlePassword.compare(passwordText, user.password)
 
     if (!isPasswordMatch) {
         return null
     }
 
-    return token.tokenSign(user)
+    return handleToken.tokenSign(user)
 }
 
 
